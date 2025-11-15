465265ae8e26c38aa760396c4d238b4d70e92bbc
Arce Ezequiel <167578938+EzequielArce@users.noreply.github.com>
2025-11-15 18:02:31 -0300

    Correcion idSector

--- 08_procedures/detalle_camas_disponibles_procedure.sql
+++ 08_procedures/detalle_camas_disponibles_procedure.sql
@@ -15,7 +15,7 @@ BEGIN
             INNER JOIN Habitacion h ON s.idSector = h.idSector
             INNER JOIN Cama c ON h.idHabitacion = c.idHabitacion
         WHERE c.ocupada = 0
-        ORDER BY s.idHabitacion, s.descripcion, h.idHabitacion, c.nroCama;
+        ORDER BY s.idSector, s.descripcion, h.idHabitacion, c.nroCama;
     END TRY
     BEGIN CATCH
         THROW;  -- Reenvía el error al cliente
